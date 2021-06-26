9a5bc0d3ed9b85f4d3a7af629c924ff6ff5da9ab
Tung <tungcan2000@gmail.com>
2021-06-26 17:07:44 +0700

    bt

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -424,93 +424,9 @@
     <script src="assets\libs\raphael\raphael.min.js"></script>
     <!-- Sparkline charts -->
     <script src="assets\libs\jquery-sparkline\jquery.sparkline.min.js"></script>
-    <!-- Dashboard init JS -->
-    <script src="assets\js\pages\dashboard.init.js"></script>
     <!-- App js -->
     <script src="assets\js\app.min.js"></script>
-
-    <script>
-        $(".conversation-list").scrollTo("100%", "100%", {
-            easing: "swing"
-        });
-        $('.slimScrollBar').css('bottom','0').css('top','');
-        let switch_them = theme==1 ? $("#dark-mode-switch").click():'';
-
-        $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
-
-        $(document).on('change',"#dark-mode-switch, #light-mode-switch",function(){
-
-            theme = $("#dark-mode-switch").prop('checked') ? 1:0;
-
-                 $.ajax({
-                    type: 'POST',
-                    url: upSetUrl,
-                    dataType: 'json',
-                    contentType: 'application/json',
-                    data: JSON.stringify({
-                        'theme': theme,
-                    }),
-                    success: function() {
-
-                    }
-                })
-        })
-        $('#document-post').on('click', function() {
-            let t = $('#document-title').val();
-            let c = $('#document-content').val();
-            if(!t || !c || t.trim() == '' || c.trim() == '')
-                sweetAlert("Không đủ thông tin", "error");
-            else
-                $.ajax({
-                    type: 'POST',
-                    url: postDocUrl,
-                    dataType: 'json',
-                    contentType: 'application/json',
-                    data: JSON.stringify({
-                        'title': t,
-                        'content': c
-                    }),
-                    success: function() {
-                        $('#document-title').val('');
-                        $('#document-content').val('');
-                    }
-                })
-        });
-        $('#random-number-button').on('click', function() {
-            let min_element = $('input#min-number');
-            let max_element = $('input#max-number');
-            let generated_number__element = $('#generated-number');
-
-            let max = Math.floor(parseInt(max_element.val()) + 1);
-            let min = Math.ceil(parseInt(min_element.val()));
-
-            //let generated_number = Math.floor(Math.random() * (max - min) + min);
-            if(min<=max ){
-                generated_number__element.val(Math.floor(Math.random() * (max - min) + min));
-                $.ajax({
-                    type: 'GET',
-                    url: nbAddUrl,
-                    success: function() {
-                    }
-                })
-            }
-        });
-
-        $('.copy-btn').on('click', function() {
-
-            let cpy = $('<input>');
-
-            $('body').append(cpy);
-
-            cpy.val($($(this).attr('target')).text().trim()).select();
-
-            document.execCommand('copy');
-
-            cpy.remove();
-        })
-
-        $('.category-link:first-child').addClass('active');
-        $('div[role="tabpanel"]:first-child').addClass('active');
-    </script>
+    <!-- Dashboard init JS -->
+    <script src="assets\js\pages\dashboard.init.js"></script>
   </body>
 </html>
