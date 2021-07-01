b214ea9b9baa318fc7bf5450d679dbad774e60f7
Tung <tungcan2000@gmail.com>
2021-07-01 11:14:41 +0700

    bt

--- public/assets/js/pages/dashboard.init.js
+++ public/assets/js/pages/dashboard.init.js
@@ -99,13 +99,33 @@ $('#document-search').on('keyup', function() {
     !$('#vanmau-section .category-link#v-all-tab').hasClass('active') ?
         $('#vanmau-section .category-link#v-all-tab').click() : '';
 
-    let key = $(this).val().replace(/ /g, '').toLowerCase();
+    let key = formatText($(this).val());
     $('.tab-pane#v-all #accordion-all .card').css('display', 'none').filter(function(id, ele) {
 
-        return $(ele).find('.card-header h6').html().replace(/ /g, '').toLowerCase().includes(key) || $(ele).find('.card-body.document-content').html().replace(/ /g, '').toLowerCase().includes(key);
+        return formatText($(ele).find('.card-header h6').html()).includes(key) || formatText($(ele).find('.card-body.document-content').html()).includes(key);
 
     }).css('display', 'block');
 })
+
 $('.document-title').css('padding', '.7rem 1rem');
 $('.category-link:first-child').addClass('active');
 $('div[role="tabpanel"]:first-child').addClass('active');
+
+function formatText(str) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    str = str.replace(/ /g, '').toLowerCase();
+    return str;
+}
