a71930514675556fc7e5a83a57dfadece1d9a33e
Tung <tungcan2000@gmail.com>
2021-07-08 11:56:27 +0700

    bt

--- public/assets/js/pages/react.js
+++ public/assets/js/pages/react.js
@@ -11,15 +11,24 @@ const SetCountDown = (t) => {
     return o + c + r + i + s + l;
 };
 const formatTime = (a) => {
-    return new moment(a, "YYYY-MM-DD hh:mm:ss").locale('vi').format('dddd, MMMM Do YYYY, h:mm:ss a');
-}
+    return new moment(a, "YYYY-MM-DD hh:mm:ss")
+        .locale("vi")
+        .format("dddd, MMMM Do YYYY, h:mm:ss a");
+};
 const CountDown = (props) => {
     return (
         <div className="col-xl-4">
             <div className="page-title-box">
-                <div className="count-down" style={{backgroundImage : `gradient( rgb(0 0 0 / 0%), rgba(0, 0, 0, 0.7) ),url(${props.background})`}}>
+                <div
+                    className="count-down"
+                    style={{
+                        background: `linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ),url(${props.background})`,
+                    }}
+                >
                     <div className="count-down__title">{props.name}</div>
-                    <div className="count-down__endTime">{formatTime(props.time)}</div>
+                    <div className="count-down__endTime">
+                        {formatTime(props.time)}
+                    </div>
                     <div endtime={props.time} className="count-down__time">
                         {SetCountDown(props.time)}
                     </div>
@@ -38,17 +47,18 @@ $.ajax({
         countdowns = data;
     },
 }),
-setInterval(function () {
-
-    elements = countdowns.map(countdown => {
-        return <CountDown
-            name= {countdown.title}
-            time= {countdown.endTime}
-            background={countdown.background}
-        />;
-    }),
-    ReactDOM.render(
-        elements,
-        document.getElementById("countdown-section")
-    );
-}, 1e3);
+    setInterval(function () {
+        (elements = countdowns.map((countdown) => {
+            return (
+                <CountDown
+                    name={countdown.title}
+                    time={countdown.endTime}
+                    background={countdown.background}
+                />
+            );
+        })),
+            ReactDOM.render(
+                elements,
+                document.getElementById("countdown-section")
+            );
+    }, 1e3);
